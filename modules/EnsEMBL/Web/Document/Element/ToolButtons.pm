555b4978c4661b25ab8ee3e39372105cdeabc4da
Eugene Kulesha <ek@ebi.ac.uk>
2014-10-15 17:43:23 +0100

    up to e76

--- modules/EnsEMBL/Web/Document/Element/ToolButtons.pm
+++ modules/EnsEMBL/Web/Document/Element/ToolButtons.pm
@@ -1,6 +1,31 @@
-# $Id: ToolButtons.pm,v 1.1.18.1 2014-09-12 09:39:14 jk10 Exp $
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
 
 package EnsEMBL::Web::Document::Element::ToolButtons;
+
+# Generates the tools buttons below the left menu - configuration, data export, etc.
+
+use strict;
+
+use URI::Escape qw(uri_escape);
+
+use base qw(EnsEMBL::Web::Document::Element);
+
 
 sub label_classes {
   return {
@@ -14,6 +39,7 @@ sub label_classes {
   };
 }
 
+
 sub init {
   my $self       = shift;  
   my $controller = shift;
@@ -23,9 +49,12 @@ sub init {
   my $session    = $hub->session;
   my $user       = $hub->user;
   my $has_data   = grep($session->get_data(type => $_), qw (upload url das)) || ($user && (grep $user->get_records($_), qw(uploads urls dases)));
+
+  my $view_config;
+     $view_config = $hub->get_viewconfig(@{shift @components}) while !$view_config && scalar @components; 
+
   my $url         = $hub->url;
   my $region      = $hub->param('r') || '';  
-  my $view_config = $hub->get_viewconfig(@{shift @components}) while !$view_config && scalar @components; 
   
   if ($view_config) {
     my $component = $view_config->component;
@@ -75,7 +104,7 @@ sub init {
       title   => 'You cannot export data from this page'
     });
   }
-
+  
   #vcf BOF                                                                                                                                                                                                   
   if (($url !~ /Info\/Index/) && ($region =~ /^(.+?):(\d+)-(\d+)$/)) {
     $self->add_entry({
@@ -98,7 +127,8 @@ sub init {
       });
   }
   #vcf EOF                                 
-  
+
+
   if ($hub->user) {
     my $title = $controller->page->title;
     
