ea09b170c505e6a90c7051daf98048ad6403a45c
Eugene Kulesha <ek@ebi.ac.uk>
2014-10-22 11:21:24 +0100

    data have been fixed - re-enable the resequencing view

--- modules/EnsEMBL/Web/Configuration/Location.pm
+++ modules/EnsEMBL/Web/Configuration/Location.pm
@@ -6,7 +6,6 @@ sub modify_tree {
     
     $self->delete_node('Compara');
     $self->delete_node('OtherBrowsers');
-    $self->delete_node('SequenceAlignment'); # the view does not seem to work at all , even at grch37.ensembl.org
 }
 
 1;
