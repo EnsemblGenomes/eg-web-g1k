3d691e34405d72d29e454d672a65499395fbb968
Julia Khobova <jk@ebi.ac.uk>
2014-11-17 15:22:05 +0000

    ensembl-3537: removed the Display your data in 1000 Genomes and the More about the 1000 Genomes regulatory build links changed the ftp link

--- modules/EnsEMBL/Web/Component/Info/HomePage.pm
+++ modules/EnsEMBL/Web/Component/Info/HomePage.pm
@@ -85,6 +85,64 @@ sub content {
   return $html;  
 }
 
+sub _assembly_text {
+  my $self             = shift;
+  my $hub              = $self->hub;
+  my $species_defs     = $hub->species_defs;
+  my $species          = $hub->species;
+  my $name             = $species_defs->SPECIES_COMMON_NAME;
+  my $img_url          = $self->img_url;
+  my $sample_data      = $species_defs->SAMPLE_DATA;
+  my $ensembl_version  = $self->_site_release;
+  my $current_assembly = $species_defs->ASSEMBLY_NAME;
+  my $accession        = $species_defs->ASSEMBLY_ACCESSION;
+  my $source           = $species_defs->ASSEMBLY_ACCESSION_SOURCE || 'NCBI';
+  my $source_type      = $species_defs->ASSEMBLY_ACCESSION_TYPE;
+ #my %archive          = %{$species_defs->get_config($species, 'ENSEMBL_ARCHIVES') || {}};
+  my %assemblies       = %{$species_defs->get_config($species, 'ASSEMBLIES') || {}};
+  my $previous         = $current_assembly;
+
+  my $html = '<div class="homepage-icon">';
+
+  if (@{$species_defs->ENSEMBL_CHROMOSOMES || []}) {
+    $html .= qq(<a class="nodeco _ht" href="/$species/Location/Genome" title="Go to $name karyotype"><img src="${img_url}96/karyotype.png" class="bordered" /><span>View karyotype</span></a>);
+  }
+
+  my $region_text = $sample_data->{'LOCATION_TEXT'};
+  my $region_url  = $species_defs->species_path . '/Location/View?r=' . $sample_data->{'LOCATION_PARAM'};
+
+  $html .= qq(<a class="nodeco _ht" href="$region_url" title="Go to $region_text"><img src="${img_url}96/region.png" class="bordered" /><span>Example region</span></a>);
+  $html .= '</div>'; #homepage-icon
+
+  my $assembly = $current_assembly;
+  if ($accession) {
+    $assembly = $hub->get_ExtURL_link($current_assembly, 'ENA', $accession);
+  }
+  $html .= "<h2>Genome assembly: $assembly</h2>";
+  $html .= qq(<p><a href="/$species/Info/Annotation/#assembly" class="nodeco"><img src="${img_url}24/info.png" alt="" class="homepage-link" />More information and statistics</a></p>);
+
+  # Link to FTP site
+  if ($species_defs->ENSEMBL_FTP_URL) {
+    my $ftp_url;
+    if ($self->is_bacteria) {
+      $ftp_url = sprintf '%s/release-%s/fasta/%s_collection/%s/dna/', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, $species_defs->SPECIES_DATASET, lc $species;
+    }
+    else {
+      $ftp_url = sprintf '%s/release-%s/fasta/%s/dna/', $species_defs->ENSEMBL_FTP_URL, $ensembl_version, lc $species;
+    }
+    $html .= qq(<p><a href="$ftp_url" class="nodeco"><img src="${img_url}24/download.png" alt="" class="homepage-link" />Download DNA sequence</a> (FASTA)</p>);
+  }
+
+  # Link to assembly mapper
+  my $mappings = $species_defs->ASSEMBLY_MAPPINGS;
+  if ($mappings && ref($mappings) eq 'ARRAY') {
+    my $am_url = $hub->url({'type' => 'UserData', 'action' => 'SelectFeatures'});
+    $html .= qq(<p><a href="$am_url" class="modal_link nodeco"><img src="${img_url}24/tool.png" class="homepage-link" />Convert your data to $assembly coordinates</a></p>);
+  }
+  
+  return $html;
+}
+
 sub _variation_text {
   my $self            = shift;
   my $hub             = $self->hub;
