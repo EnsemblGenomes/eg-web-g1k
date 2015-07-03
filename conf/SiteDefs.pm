bc2caf7016956527af873a4110dc6cd8701408b4
Julia Khobova <jk@ebi.ac.uk>
2015-07-03 14:51:46 +0100

    ensembl-3937 Release version and date

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -6,8 +6,8 @@ sub update_conf {
     $SiteDefs::ENSEMBL_PROXY_PORT = 80;
     $SiteDefs::ENSEMBL_SITETYPE = '1000 Genomes';
     $SiteDefs::SITE_NAME = '1000 Genomes';
-    $SiteDefs::SITE_RELEASE_VERSION = 16;
-    $SiteDefs::SITE_RELEASE_DATE = 'Oct 2014';
+    $SiteDefs::SITE_RELEASE_VERSION = 17;
+    $SiteDefs::SITE_RELEASE_DATE = 'Jul 2015';
     $SiteDefs::ENSEMBL_SERVERNAME     = 'browser.1000genomes.org';
     $SiteDefs::ENSEMBL_BASE_URL     = 'http://browser.1000genomes.org';
     $SiteDefs::ENSEMBL_HELPDESK_EMAIL  = 'info@1000genomes.org';
